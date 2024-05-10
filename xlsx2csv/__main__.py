c68b4391662cb052daf256ed51b6f61275484596
georgeker <geokeratz@gmail.com>
2024-05-10 10:51:54 +0300

    argparse library

--- xlsx2csv/__main__.py
+++ xlsx2csv/__main__.py
@@ -1,16 +1,22 @@
-import pandas as pd
+import argparse
 import sys
+import pandas as pd
+
+parser = argparse.ArgumentParser(description='Convert xlsx file to csv')
+parser.add_argument('file', type=str, help='xlsx file to convert')
+parser.add_argument('--verbose', action='store_true', help='Print verbose messages')
+args = parser.parse_args()
 
 def main():
-    args = sys.argv[1:]
-    assert len(args) == 1, 'Please provide a file name'
-    
-    xlsx_file = args[0]
-    print(f'Converting file: {xlsx_file} to csv')
+    assert args.file.endswith('.xlsx'), 'File must be xlsx format'
+
+    xlsx_file = args.file
+    if args.verbose: print(f'Converting file: {xlsx_file} to csv')
 
     for sheet in pd.read_excel(xlsx_file, sheet_name=None, dtype=str):
         print(sheet)
-        print(pd.read_excel(xlsx_file, sheet_name=sheet, dtype=str))
+        if args.verbose: print(pd.read_excel(xlsx_file, sheet_name=sheet, dtype=str).head())
+
         pd.read_excel(xlsx_file, sheet_name=sheet).to_csv(f'{xlsx_file}_{sheet}.csv', index=False)
         print(f'File {xlsx_file}_{sheet}.csv has been created')
 
